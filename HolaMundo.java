85c0f190e5cc8d3982a7faa4cca7a5970297369e
MontesJMA <josejma14@gmail.com>
2020-02-03 09:52:32 +0100

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
+	String naranja = "\033[33m";
+	String azul = "\033[34m";
 		System.out.println("Hola Mundo");
 		System.out.println("Ampliacion del hola");
 	}
